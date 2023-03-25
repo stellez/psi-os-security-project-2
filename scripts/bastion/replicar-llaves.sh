a8598e1a2dd7d95929b3e4c0eb72783da7fd5189
Steven Tellez <stevenz@galileo.edu>
2023-03-25 17:41:11 -0600

    additional improvements

--- scripts/bastion/replicar-llaves.sh
+++ scripts/bastion/replicar-llaves.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-apt install sshpass -y
+REQUIRED_PKG="sshpass"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+  sudo apt-get --yes install $REQUIRED_PKG
+fi
 su - web01 -c "sshpass -p psi-web-01 scp -o StrictHostKeyChecking=accept-new -r .ssh/ web01@10.0.0.2:~"
 su - web02 -c "sshpass -p psi-web-02 scp -o StrictHostKeyChecking=accept-new -r .ssh/ web02@10.0.0.2:~"
 su - dba01 -c "sshpass -p psi-dba-01 scp -o StrictHostKeyChecking=accept-new -r .ssh/ dba01@10.0.0.3:~"
