a4d884f7dd473429b3af0194b07055a225eb3828
Steven Tellez <stevenz@galileo.edu>
2023-03-25 16:37:42 -0600

    StrictHostKeyChecking added on the fly for virtual terminal

--- scripts/cliente/crear-llaves.sh
+++ scripts/cliente/crear-llaves.sh
@@ -12,25 +12,25 @@ if [ "" = "$PKG_OK" ]; then
 fi
 
 su - web01 -c "ssh-keygen -b 4096 -f ~web01/.ssh/id_rsa -N 'Me encanta Linux'"
-su - web01 -c "printf 'Host 10.0.0.2\n\tStrictHostKeyChecking accept-new\nProxyJump $BASTION_HOST\n' >> ~/.ssh/config"
-su - web01 -c "echo 'psi-web-01' > pass-file & chmod 0400 pass-file & sshpass -f pass-file ssh-copy-id web01@$BASTION_HOST"
+su - web01 -c "echo 'psi-web-01' > pass-file & chmod 0400 pass-file & sshpass -f pass-file ssh-copy-id -o StrictHostKeyChecking=accept-new web01@$BASTION_HOST"
+su - web01 -c "printf 'Host 10.0.0.2\n\tProxyJump $BASTION_HOST\n' >> ~/.ssh/config"
 
 su - web02 -c "ssh-keygen -b 4096 -f ~web02/.ssh/id_rsa -N 'Me encanta Linux'"
-su - web02 -c "printf 'Host 10.0.0.2\n\tStrictHostKeyChecking accept-new\nProxyJump $BASTION_HOST\n' >> ~/.ssh/config"
-su - web02 -c "echo 'psi-web-02' > pass-file & chmod 0400 pass-file & sshpass -f pass-file ssh-copy-id web02@$BASTION_HOST"
+su - web02 -c "echo 'psi-web-02' > pass-file & chmod 0400 pass-file & sshpass -f pass-file ssh-copy-id -o StrictHostKeyChecking=accept-new web02@$BASTION_HOST"
+su - web02 -c "printf 'Host 10.0.0.2\n\tProxyJump $BASTION_HOST\n' >> ~/.ssh/config"
 
 su - dba01 -c "ssh-keygen -b 4096 -f ~dba01/.ssh/id_rsa -N 'Me encanta Linux'"
-su - dba01 -c "printf 'Host 10.0.0.3\n\tStrictHostKeyChecking accept-new\nProxyJump $BASTION_HOST\n' >> ~/.ssh/config"
-su - dba01 -c "echo 'psi-dba-01' > pass-file & chmod 0400 pass-file & sshpass -f pass-file ssh-copy-id dba01@$BASTION_HOST"
+su - dba01 -c "echo 'psi-dba-01' > pass-file & chmod 0400 pass-file & sshpass -f pass-file ssh-copy-id -o StrictHostKeyChecking=accept-new dba01@$BASTION_HOST"
+su - dba01 -c "printf 'Host 10.0.0.3\n\tProxyJump $BASTION_HOST\n' >> ~/.ssh/config"
 
 su - dba02 -c "ssh-keygen -b 4096 -f ~dba02/.ssh/id_rsa -N 'Me encanta Linux'"
-su - dba02 -c "printf 'Host 10.0.0.3\n\tStrictHostKeyChecking accept-new\nProxyJump $BASTION_HOST\n' >> ~/.ssh/config"
-su - dba02 -c "echo 'psi-dba-02' > pass-file & chmod 0400 pass-file & sshpass -f pass-file ssh-copy-id dba02@$BASTION_HOST"
+su - dba02 -c "echo 'psi-dba-02' > pass-file & chmod 0400 pass-file & sshpass -f pass-file ssh-copy-id -o StrictHostKeyChecking=accept-new dba02@$BASTION_HOST"
+su - dba02 -c "printf 'Host 10.0.0.3\n\tProxyJump $BASTION_HOST\n' >> ~/.ssh/config"
 
 su - adm01 -c "ssh-keygen -b 4096 -f ~adm01/.ssh/id_rsa -N 'Me encanta Linux'"
-su - adm01 -c "printf 'Host 10.0.0.2\n\tStrictHostKeyChecking accept-new\nProxyJump $BASTION_HOST\n\rHost 10.0.0.3\n\tStrictHostKeyChecking accept-new\nProxyJump $BASTION_HOST\n' >> ~/.ssh/config"
-su - adm01 -c "echo 'psi-adm-01' > pass-file & chmod 0400 pass-file & sshpass -f pass-file ssh-copy-id adm01@$BASTION_HOST"
+su - adm01 -c "echo 'psi-adm-01' > pass-file & chmod 0400 pass-file & sshpass -f pass-file ssh-copy-id -o StrictHostKeyChecking=accept-new adm01@$BASTION_HOST"
+su - adm01 -c "printf 'Host 10.0.0.2\n\tProxyJump $BASTION_HOST\n\rHost 10.0.0.3\n\tProxyJump $BASTION_HOST\n' >> ~/.ssh/config"
 
 su - adm02 -c "ssh-keygen -b 4096 -f ~adm02/.ssh/id_rsa -N 'Me encanta Linux'"
-su - adm02 -c "printf 'Host 10.0.0.2\n\tStrictHostKeyChecking accept-new\nProxyJump $BASTION_HOST\n\rHost 10.0.0.3\n\tStrictHostKeyChecking accept-new\nProxyJump $BASTION_HOST\n' >> ~/.ssh/config"
-su - adm02 -c "echo 'psi-adm-02' > pass-file & chmod 0400 pass-file & sshpass -f pass-file ssh-copy-id adm02@$BASTION_HOST"
+su - adm02 -c "echo 'psi-adm-02' > pass-file & chmod 0400 pass-file & sshpass -f pass-file ssh-copy-id -o StrictHostKeyChecking=accept-new adm02@$BASTION_HOST"
+su - adm02 -c "printf 'Host 10.0.0.2\n\tProxyJump $BASTION_HOST\n\rHost 10.0.0.3\n\tProxyJump $BASTION_HOST\n' >> ~/.ssh/config"
